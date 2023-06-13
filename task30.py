1198938f350cf504b5d12518aa879f3bec735296
Valeriy Fateev <Valerka9602@mail.ru>
2023-06-13 21:18:09 +0800

    Решение задачи 30

--- task30.py
+++ task30.py
@@ -10,3 +10,24 @@ a(n) = a(1) + (n-1) * d. Каждое число вводится с новой 
 '''
 
 
+def read_int(text):
+
+    value = int(input(text))
+    return value
+
+
+def fill_arithmetic_progression(length, initial_term, difference):
+
+    arith_list = list()
+
+    for i in range(length):
+        arith_list.append(initial_term + i * difference)
+    
+    return arith_list
+
+
+a_1 = read_int('Укажите первый элемент арифметической прогрессии: ')
+d = read_int('Укажите разность арифметической прогрессии: ')
+size = read_int('Укажите количество элементов в арифметической прогрессии: ')
+
+print(fill_arithmetic_progression(size, a_1, d))
